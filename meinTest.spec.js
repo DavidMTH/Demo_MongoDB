3cf98db3dfd1f6db5226acf834d80aae647d0c47
DaMoTh2076 <146932113+DavidMTH@users.noreply.github.com>
2024-03-01 23:47:10 +0100

    test

--- meinTest.spec.js
+++ meinTest.spec.js
@@ -1,13 +1,13 @@
-const config = {
-    projects: [
-      {
-        name: 'My Project',
-        use: {
-          command: 'npm start & npx wait-on http://localhost:3000',
-        },
-      },
-    ],
-  };
-  
-  module.exports = config;
-  
+const { test, expect } = require('@playwright/test');
+
+test('Überprüfe die Seitentitel', async ({ page }) => {
+  await page.goto('https://localhost:3000');
+  const title = await page.title();
+  expect(title).toBe('Beispiel Domain');
+});
+
+test('Überprüfe die Anzeige von Elementen', async ({ page }) => {
+  await page.goto('https://localhost:3000');
+  const element = await page.$('.example-selector');
+  expect(element).not.toBeNull();
+});
