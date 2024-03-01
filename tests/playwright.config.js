e885831ab03f520897141c388815c4d4456ee79b
DaMoTh2076 <146932113+DavidMTH@users.noreply.github.com>
2024-03-01 23:28:57 +0100

    test

--- tests/playwright.config.js
+++ tests/playwright.config.js
@@ -1,29 +1,14 @@
-/**
- * @see https://playwright.dev/docs/test-configuration
- * @type {import('@playwright/test').PlaywrightTestConfig}
- */
+const { PlaywrightTestConfig } = require('@playwright/test');
 
 const config = {
-    testDir: './tests',
-    /* Maximum time one test can run for. */
-    timeout: 30 * 1000,
-    expect: {
-      /**
-       * Maximum time expect() should wait for the condition to be met.
-       * For example in `await expect(locator).toHaveText();`
-       */
-      timeout: 5000,
+  projects: [
+    {
+      name: 'My Project',
+      use: {
+        command: 'npm start & npx wait-on http://localhost:3000',
+      },
     },
-    fullyParallel: true,
-    workers: 1,
-    /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-    reporter: 'html',
-    /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
-    webServer: {
-      command: 'npm run start',
-      url: 'http://127.0.0.1:3000',
-      reuseExistingServer: process.env.CI
-    },
-  };
-  
-  export default config;
+  ],
+};
+
+module.exports = config;
