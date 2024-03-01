6f45968f8a8ca533cda34c698321066a74cf485b
DaMoTh2076 <146932113+DavidMTH@users.noreply.github.com>
2024-03-01 23:44:34 +0100

    test

---
+++ meinTest.spec.js
@@ -0,0 +1,13 @@
+const config = {
+    projects: [
+      {
+        name: 'My Project',
+        use: {
+          command: 'npm start & npx wait-on http://localhost:3000',
+        },
+      },
+    ],
+  };
+  
+  module.exports = config;
+  
